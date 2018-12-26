905710cd627db4fa155dddd4461adc5af0a7cb89
Mingchi <ceachother@live.cn>
2018-12-26 17:43:10 -0400

    Add description in heap sort

--- src/sort/algorithm/No3_2_HeapSort.java
+++ src/sort/algorithm/No3_2_HeapSort.java
@@ -1,5 +1,7 @@
 package sort.algorithm;
 
+import static sort.algorithm.Util.swap;
+
 /**
  * Created by cecillee on 18/5/2017.
  * build max heap in every iteration
@@ -13,45 +15,103 @@ public class No3_2_HeapSort {
         Util.printArray(a);
     }
 
-    private static void sort(int[] a)
-    {
-        for(int i = a.length-1 ; i > 0; i--)
-        {
-            //build the maximum heap
-            buildMaxHeap(a,i);
-            //swap the top element of heap to the last
-            Util.swap(a,0,i);
+    public static void sort(int[] a) {
+        for(int i = a.length-1; i>=0; i--) {
+            buildHeap(a, i);
+            swap(a, i, 0);
         }
     }
 
-    private static void buildMaxHeap(int[] a , int lastPtr)
-    {
-        for(int i = lastPtr; i > 0 ; i--)
-        {
-            //start from the last node and its parent
-            int parent = (i-1)/2;
-            int child;
-            //if parent swap with the child, the child should compare to its child
-            //parent*2+1(which is his left child) can not over the size of the heap(the array)
-            for (; parent * 2 + 1 < lastPtr; parent = child) {
-                child = parent * 2 + 1;
-                //if the left child is the last element(which child = lastPtr)
-                //then left child is the largest child
-                if (child < lastPtr &&
-                        a[child + 1] > a[child]) {
-                    child++;
+    /**
+     * When buiding heap(either max or min), it has to be starting from the bottom to top
+     * @param a
+     * @param end
+     */
+    public static void buildHeap(int[] a, int end) {
+        for (int i = end; i>0; i--) {
+            int k = i/2;
+            while (k <= end) {
+                int largest = k;
+                if (k * 2 + 1 <= end && a[k * 2 + 1] > a[largest]) {
+                    largest = k * 2 + 1;
                 }
-                if (a[child] > a[parent]) {
-                    Util.swap(a, child, parent);
+                if (k * 2 + 2 <= end && a[k * 2 + 2] > a[largest]) {
+                    largest = k * 2 + 2;
                 }
-                //if parent is the largest, then no need to build the maximum child heap
-                //because the process is from bottom to top, so the children are already
-                //bigger than their children
-                else {
-                    break;
-                }
+                if (largest == k) break;
+                swap(a, largest, k);
+                k = largest;
             }
         }
     }
+
+
+//    private static void siftdown(int[] A, int k) {
+//        while (k < A.length) {
+//            int smallest = k;
+//            if (k * 2 + 1 < A.length && A[k * 2 + 1] < A[smallest]) {
+//                smallest = k * 2 + 1;
+//            }
+//            if (k * 2 + 2 < A.length && A[k * 2 + 2] < A[smallest]) {
+//                smallest = k * 2 + 2;
+//            }
+//            if (smallest == k) {
+//                break;
+//            }
+//            int temp = A[smallest];
+//            A[smallest] = A[k];
+//            A[k] = temp;
+//
+//            k = smallest;
+//        }
+//    }
+
+//    public static void heapify(int[] A) {
+//        for (int i = A.length / 2; i >= 0; i--) {
+//            siftdown(A, i);
+//        }
+//    }
+//
+//
+//    private static void sort(int[] a)
+//    {
+//        for(int i = a.length-1 ; i > 0; i--)
+//        {
+//            //build the maximum heap
+//            buildMaxHeap(a,i);
+//            //swap the top element of heap to the last
+//            Util.swap(a, 0, i);
+//        }
+//    }
+//
+//    private static void buildMaxHeap(int[] a , int lastPtr)
+//    {
+//        for(int i = lastPtr; i > 0 ; i--)
+//        {
+//            //start from the last node and its parent
+//            int parent = (i-1)/2;
+//            int child;
+//            //if parent swap with the child, the child should compare to its child
+//            //parent*2+1(which is his left child) can not over the size of the heap(the array)
+//            for (; parent * 2 + 1 < lastPtr; parent = child) {
+//                child = parent * 2 + 1;
+//                //if the left child is the last element(which child = lastPtr)
+//                //then left child is the largest child
+//                if (child < lastPtr &&
+//                        a[child + 1] > a[child]) {
+//                    child++;
+//                }
+//                if (a[child] > a[parent]) {
+//                    Util.swap(a, child, parent);
+//                }
+//                //if parent is the largest, then no need to build the maximum child heap
+//                //because the process is from bottom to top, so the children are already
+//                //bigger than their children
+//                else {
+//                    break;
+//                }
+//            }
+//        }
+//    }
 
 }
